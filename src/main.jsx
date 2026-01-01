d82dade83ef70f8edfd18fa653a265f2c623a7bb
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2026-01-01 16:29:13 +0530

    basic router code initialized

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,19 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./utils/store.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
