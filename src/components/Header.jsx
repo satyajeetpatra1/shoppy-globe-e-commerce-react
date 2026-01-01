88c7b78ed844fb44255cccb33096716d80aea1ec
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2026-01-02 00:15:36 +0530

    Header style updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,14 @@ function Header() {
   const dispatch = useDispatch();
 
   return (
-    <header className="bg-white shadow p-4 flex justify-between">
-      <Link to="/" className="text-2xl font-bold">ShoppyGlobe 🌍</Link>
+    <header className="bg-indigo-600 shadow p-4 flex justify-between">
+      <Link to="/" className="text-2xl text-white font-bold my-auto">ShoppyGlobe 🌍</Link>
       <input
         placeholder="Search products..."
-        className="border px-3 py-1 rounded"
+        className="border px-3 py-1 rounded outline-white border-white text-white placeholder:text-gray-300 my-auto"
         onChange={(e) => dispatch(setSearch(e.target.value))}
       />
-      <Link to="/cart">🛒 Cart</Link>
+      <Link className="text-white font-semibold text-md my-auto" to="/cart">🛒 Cart</Link>
     </header>
   );
 }
