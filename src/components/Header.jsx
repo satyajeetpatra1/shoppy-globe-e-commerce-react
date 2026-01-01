db39ef4d1392af25d89103254fdcdeacdba05de5
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2026-01-01 17:15:33 +0530

    Header code modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,21 @@
+import { useDispatch } from "react-redux";
+import { setSearch } from "../utils/CartSlice";
+import { Link } from "react-router-dom";
+
 function Header() {
-  return <div>Header</div>;
+  const dispatch = useDispatch();
+
+  return (
+    <header className="bg-white shadow p-4 flex justify-between">
+      <Link to="/" className="text-2xl font-bold">ShoppyGlobe 🌍</Link>
+      <input
+        placeholder="Search products..."
+        className="border px-3 py-1 rounded"
+        onChange={(e) => dispatch(setSearch(e.target.value))}
+      />
+      <Link to="/cart">🛒 Cart</Link>
+    </header>
+  );
 }
 
 export default Header;
