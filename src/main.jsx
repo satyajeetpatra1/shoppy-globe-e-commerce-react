5b8eb3eb0d3fa1a39fe011ea49c2d9bde5974231
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2026-01-02 00:32:47 +0530

    Route for ProductDetail page added

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { store } from "./utils/store.js";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import ProductDetail from "./pages/ProductDetail.jsx";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/product/:id",
+        element: <ProductDetail />,
       },
     ],
   },
