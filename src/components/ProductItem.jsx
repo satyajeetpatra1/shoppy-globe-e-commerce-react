18d9751c7b606ca28406c5694f6eb54ccfe076df
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2026-01-02 23:43:18 +0530

    simplified add remove items from cart for better UX

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,6 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import {
+  addToCart,
+  decreaseQty,
+  increaseQty,
+  removeFromCart,
+} from "../utils/CartSlice";
 
 export default function ProductItem({ product }) {
+  const dispatch = useDispatch();
+
+  const cartItems = useSelector((state) => state.cart.items);
+  const cartItem = cartItems.find((item) => item.id == product.id);
+
   return (
     <div className="bg-white p-4 rounded shadow">
       <img
@@ -14,7 +26,39 @@ export default function ProductItem({ product }) {
         <Link to={`/product/${product.id}`} className="text-blue-500">
           View
         </Link>
-        <button className="bg-black text-white px-3 py-1 rounded">Add</button>
+        {!cartItem ? (
+          <button
+            onClick={() => dispatch(addToCart(product))}
+            className="bg-black text-white px-3 py-1 rounded"
+          >
+            Add
+          </button>
+        ) : (
+          <div className="flex items-center gap-4">
+
+            <button
+              onClick={() =>
+                dispatch(
+                  cartItem.quantity === 1
+                    ? removeFromCart(product.id)
+                    : decreaseQty(product.id)
+                )
+              }
+              className="px-4 py-2 bg-gray-200 rounded-lg hover:bg-gray-300 disabled:bg-gray-50"
+            >
+              {cartItem.quantity === 1 ? "delete" : "-"}
+            </button>
+
+            <span className="text-lg font-semibold">{cartItem.quantity}</span>
+
+            <button
+              onClick={() => dispatch(increaseQty(product.id))}
+              className="px-4 py-2 bg-gray-200 rounded-lg hover:bg-gray-300"
+            >
+              +
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
