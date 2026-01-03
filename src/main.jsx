ebe3a584f4780ddd9a6e6f9f4b3602b6458e8450
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2026-01-03 12:55:26 +0530

    lazy loading added

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from "react";
+import { lazy, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./utils/store.js";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import NotFound from "./pages/NotFound.jsx";
-import ProductDetail from "./pages/ProductDetail.jsx";
-import Cart from "./pages/Cart.jsx";
-import Checkout from "./pages/Checkout.jsx";
+
+// lazy loading
+const Home = lazy(() => import("./pages/Home.jsx"));
+const NotFound = lazy(() => import("./pages/NotFound.jsx"));
+const ProductDetail = lazy(() => import("./pages/ProductDetail.jsx"));
+const Cart = lazy(() => import("./pages/Cart.jsx"));
+const Checkout = lazy(() => import("./pages/Checkout.jsx"));
 
 const router = createBrowserRouter([
   {
